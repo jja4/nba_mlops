ab9499031ed1cabb6ea6202a8f34802f19877235
Vince030 <129843213+Vince030@users.noreply.github.com>
2024-06-11 18:06:48 +0800

    Added app.post endpoint for prediction

--- code/api/nba_app.py
+++ code/api/nba_app.py
@@ -3,8 +3,10 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime, timedelta
-import jwt
 from passlib.context import CryptContext
+import jwt
+import pandas as pd
+import joblib
 
 # Constants
 SECRET_KEY = "your_secret_key"
@@ -143,3 +145,102 @@ async def predict(data: dict, current_user: User = Depends(get_current_user)):
 def predict_with_model(data):
     # Replace with actual machine learning model
     return "This is a dummy prediction"
+
+
+class ScoringItem(BaseModel):
+    Period: int
+    Minutes_Remaining: int
+    Seconds_Remaining: int
+    Shot_Distance: int
+    X_Location: int
+    Y_Location: int
+    #Shot_Made_Flag: int
+    Action_Type_Frequency: float
+    Team_Name_Frequency: float
+    Home_Team_Frequency: float
+    Away_Team_Frequency: float
+    ShotType_2PT_Field_Goal: int
+    ShotType_3PT_Field_Goal: int
+    ShotZoneBasic_Above_the_Break_3: int
+    ShotZoneBasic_Backcourt: int
+    ShotZoneBasic_In_The_Paint_Non_RA: int
+    ShotZoneBasic_Left_Corner_3: int
+    ShotZoneBasic_Mid_Range: int
+    ShotZoneBasic_Restricted_Area: int
+    ShotZoneBasic_Right_Corner_3: int
+    ShotZoneArea_Back_Court_BC: int
+    ShotZoneArea_Center_C: int
+    ShotZoneArea_Left_Side_Center_LC: int
+    ShotZoneArea_Left_Side_L: int
+    ShotZoneArea_Right_Side_Center_RC: int
+    ShotZoneArea_Right_Side_R: int
+    ShotZoneRange_16_24_ft: int
+    ShotZoneRange_24_ft: int
+    ShotZoneRange_8_16_ft: int
+    ShotZoneRange_Back_Court_Shot: int
+    ShotZoneRange_Less_Than_8_ft: int
+    SeasonType_Playoffs: int
+    SeasonType_Regular_Season: int
+    Game_ID_Frequency: float
+    Game_Event_ID_Frequency: float
+    Player_ID_Frequency: float
+    Year: int
+    Month: int
+    Day: int
+    Day_of_Week: int
+
+
+# Load the model
+model = joblib.load('/Users/Vinc/Desktop/streamlit/models/model_best_lr.joblib')
+
+
+@app.post('/')
+async def scoring_endpoint(item: ScoringItem):
+    # Create a DataFrame with the data of the request object
+    df = pd.DataFrame([item.dict()])
+    # Rename the columns to match the expected names
+    df = df.rename(columns={
+        "Minutes_Remaining": "Minutes Remaining",
+        "Seconds_Remaining": "Seconds Remaining",
+        "Shot_Distance": "Shot Distance",
+        "X_Location": "X Location",
+        "Y_Location": "Y Location",
+        #"Shot_Made_Flag": "Shot Made Flag",
+        "Action_Type_Frequency": "Action Type Frequency",
+        "Team_Name_Frequency": "Team Name_Frequency",
+        "Home_Team_Frequency": "Home Team_Frequency",
+        "Away_Team_Frequency": "Away Team_Frequency",
+        "ShotType_2PT_Field_Goal": "ShotType_2PT Field Goal",
+        "ShotType_3PT_Field_Goal": "ShotType_3PT Field Goal",
+        "ShotZoneBasic_Above_the_Break_3": "ShotZoneBasic_Above the Break 3",
+        "ShotZoneBasic_Backcourt": "ShotZoneBasic_Backcourt",
+        "ShotZoneBasic_In_The_Paint_Non_RA": "ShotZoneBasic_In The Paint (Non-RA)",
+        "ShotZoneBasic_Left_Corner_3": "ShotZoneBasic_Left Corner 3",
+        "ShotZoneBasic_Mid_Range": "ShotZoneBasic_Mid-Range",
+        "ShotZoneBasic_Restricted_Area": "ShotZoneBasic_Restricted Area",
+        "ShotZoneBasic_Right_Corner_3": "ShotZoneBasic_Right Corner 3",
+        "ShotZoneArea_Back_Court_BC": "ShotZoneArea_Back Court(BC)",
+        "ShotZoneArea_Center_C": "ShotZoneArea_Center(C)",
+        "ShotZoneArea_Left_Side_Center_LC": "ShotZoneArea_Left Side Center(LC)",
+        "ShotZoneArea_Left_Side_L": "ShotZoneArea_Left Side(L)",
+        "ShotZoneArea_Right_Side_Center_RC": "ShotZoneArea_Right Side Center(RC)",
+        "ShotZoneArea_Right_Side_R": "ShotZoneArea_Right Side(R)",
+        "ShotZoneRange_16_24_ft": "ShotZoneRange_16-24 ft.",
+        "ShotZoneRange_24_ft": "ShotZoneRange_24+ ft.",
+        "ShotZoneRange_8_16_ft": "ShotZoneRange_8-16 ft.",
+        "ShotZoneRange_Back_Court_Shot": "ShotZoneRange_Back Court Shot",
+        "ShotZoneRange_Less_Than_8_ft": "ShotZoneRange_Less Than 8 ft.",
+        "SeasonType_Playoffs": "SeasonType_Playoffs",
+        "SeasonType_Regular_Season": "SeasonType_Regular Season",
+        "Game_ID_Frequency": "Game ID_Frequency",
+        "Game_Event_ID_Frequency": "Game Event ID_Frequency",
+        "Player_ID_Frequency": "Player ID_Frequency",
+        "Year": "Year",
+        "Month": "Month",
+        "Day": "Day",
+        "Day_of_Week": "Day_of_Week"
+    })
+    # Make a prediction with the loaded model
+    yhat = model.predict(df)
+    # Return the prediction as an answer
+    return {"prediction": int(yhat)}
