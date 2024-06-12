ee9a46e8d6d7a9386be1ba71c548ad9dfb385143
MihranD <mihrandovlatyan@gmail.com>
2024-06-12 12:47:12 +0200

    Unit tests for root api

---
+++ code/tests/test_prediction.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from api.nba_app import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the NBA prediction API!"}
