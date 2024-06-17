6c379791fc0ac4ae2bed42ec06c98fcb59b0a0c3
Vince030 <129843213+Vince030@users.noreply.github.com>
2024-06-17 19:16:49 +0800

    Added Dataset in data data/raw and implemented first class for data ingestion

--- code/retrieve_data/make_dataset.py
+++ code/retrieve_data/make_dataset.py
@@ -1,1 +1,30 @@
+import logging
+import pandas as pd
 
+class IngestData:
+    """
+    Data ingestion class which ingests data from the source and returns a DataFrame.
+    """
+
+    def __init__(self) -> None:
+        """Initialize the data ingestion class."""
+        pass
+
+    def get_data(self) -> pd.DataFrame:
+        df = pd.read_csv("./data/raw/NBA Shot Locations 1997 - 2020.csv")
+        return df
+
+def ingest_data() -> pd.DataFrame:
+    """
+    Args:
+        None
+    Returns:
+        df: pd.DataFrame
+    """
+    try:
+        ingest_data = IngestData()
+        df = ingest_data.get_data()
+        return df
+    except Exception as e:
+        logging.error(e)
+        raise e
