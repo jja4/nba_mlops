c401d22da348459d505ab2bc9e772332a2bbb1ec
Joel Aftreth <joelaftreth@gmail.com>
2024-06-21 21:17:19 +0000

    updated docker-compose.api for connecting the db and api successfully

---
+++ code/database/init.sql
@@ -0,0 +1,18 @@
+-- Check if the database exists and create it if it doesn't
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'nba_db') THEN
+        CREATE DATABASE nba_db;
+    END IF;
+END $$;
+
+-- Connect to the nba_db database
+\c nba_db
+
+-- Create the users table if it doesn't exist
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL,
+    disabled BOOLEAN NOT NULL DEFAULT FALSE
+);
