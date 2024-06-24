dfd942925f4d7d1ed0507055fa8c5b8eb9ef12de
Joel Aftreth <joelaftreth@gmail.com>
2024-06-24 20:54:48 +0000

    seprated api into a gateway main api and prediction microservice

--- code/api/nba_app.py
+++ code/api/nba_app.py
@@ -4,6 +4,7 @@ from typing import Union, Optional, Dict
 import jwt
 from fastapi import Depends, FastAPI, HTTPException, status, Body
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from contextlib import asynccontextmanager
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 from typing_extensions import Annotated
@@ -18,11 +19,10 @@ from psycopg2.extras import RealDictCursor
 import time
 from psycopg2 import OperationalError
 import logging
+import httpx
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-app = FastAPI()
 
 # to get a string like this run:
 # openssl rand -hex 32
@@ -31,10 +31,18 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 # Database connection details
-DB_HOST = os.getenv("DB_HOST", "localhost")  # Default to 'db' for Docker Compose
+DB_HOST = os.getenv("DB_HOST", "localhost")  # Uses'db' for Docker Compose
 DB_NAME = os.getenv("DB_NAME", "nba_db")
 DB_USER = os.getenv("DB_USER", "ubuntu")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "mlops")
+PREDICTION_SERVICE_HOST = os.getenv('PREDICTION_SERVICE_HOST', 'localhost')
+PREDICTION_SERVICE_PORT = os.getenv('PREDICTION_SERVICE_PORT', '8001')
+
+# Configure CORS so we can communicate with the React frontend app
+origins = [
+    "http://localhost:3000",  # origin for the React app
+]
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -62,8 +70,9 @@ def get_db_connection():
     return conn
 
 
-@app.on_event("startup")
-async def startup_event():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup event
     try:
         with get_db_connection() as conn:
             with conn.cursor() as cur:
@@ -88,6 +97,21 @@ async def startup_event():
         print(f"Error during application startup: {e}")
         raise
 
+    yield  # This is where the application runs
+
+    # Shutdown event (if needed)
+    # can add any cleanup code here if necessary
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Token(BaseModel):
     access_token: str
@@ -101,7 +125,7 @@ class TokenData(BaseModel):
 class User(BaseModel):
     username: str = Field(default="testuser")
     password: str = Field(default="testpassword")
-    disabled: Union[bool, None] = None
+    disabled: Union[bool, None] = False
 
 
 class UserOut(BaseModel):
@@ -111,27 +135,6 @@ class UserOut(BaseModel):
 
 class UserInDB(User):
     hashed_password: str
-
-
-# Get the path to the project root directory
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-
-# Construct the path to the 'trained_models' directory
-trained_models_dir = os.path.join(project_root, 'trained_models')
-
-# Specify the filename of the trained model
-joblib_filename = 'model_best_lr.joblib'
-
-# Construct the full path to the joblib file
-joblib_file_path = os.path.join(trained_models_dir, joblib_filename)
-
-# Load the joblib file
-model = load(joblib_file_path)
-
-# Configure CORS so we can communicate with the React frontend app
-origins = [
-    "http://localhost:3000",  # origin for the React app
-]
 
 
 def get_user(username: str):
@@ -227,6 +230,11 @@ async def login_for_access_token(
     return Token(access_token=access_token, token_type="bearer")
 
 
+@app.get("/user", response_model=UserOut)
+async def read_current_user(
+    current_user: Annotated[User, Depends(authorize_user)],
+):
+    return UserOut(username=current_user.username, disabled=current_user.disabled)
 
 # Signup endpoint
 @app.post("/signup")
@@ -245,29 +253,17 @@ async def signup(user: User):
         cur.close()
         conn.close()
 
-@app.get("/user", response_model=UserOut)
-async def read_current_user(
-    current_user: Annotated[User, Depends(authorize_user)],
-):
-    return UserOut(username=current_user.username, disabled=current_user.disabled)
 
 
+# Gateway to Prediction Service
 @app.post("/predict")
 async def secure_predict(
     current_user: Annotated[User, Depends(authorize_user)],
 ):
-    # Perform prediction using the machine learning model
-    prediction = random.randint(0, 1)
-    return {"prediction": prediction}
-
-    
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+    url = f"http://{PREDICTION_SERVICE_HOST}:{PREDICTION_SERVICE_PORT}/predict"
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url)
+    return response.json()
 
 
 class ScoringItem(BaseModel):
@@ -314,95 +310,27 @@ class ScoringItem(BaseModel):
     Day: int
     Day_of_Week: int
 
+
+
+
 @app.post('/unsecure_predict')
 async def unsecure_predict(item: ScoringItem):
-    """
-    Endpoint for unsecure_prediction based on scoring parameters.
-
-    Args:
-        item (ScoringItem): Input parameters for prediction.
-
-    Returns:
-        dict: Prediction result, can be 1 or 0 indicating shot made or missed.
-    """
-    # Create a DataFrame with the data of the request object
-    df = pd.DataFrame([item.model_dump()])
-    # Rename the columns to match the expected names
-    df = df.rename(columns={
-        "Minutes_Remaining": "Minutes Remaining",
-        "Seconds_Remaining": "Seconds Remaining",
-        "Shot_Distance": "Shot Distance",
-        "X_Location": "X Location",
-        "Y_Location": "Y Location",
-        #"Shot_Made_Flag": "Shot Made Flag",
-        "Action_Type_Frequency": "Action Type Frequency",
-        "Team_Name_Frequency": "Team Name_Frequency",
-        "Home_Team_Frequency": "Home Team_Frequency",
-        "Away_Team_Frequency": "Away Team_Frequency",
-        "ShotType_2PT_Field_Goal": "ShotType_2PT Field Goal",
-        "ShotType_3PT_Field_Goal": "ShotType_3PT Field Goal",
-        "ShotZoneBasic_Above_the_Break_3": "ShotZoneBasic_Above the Break 3",
-        "ShotZoneBasic_Backcourt": "ShotZoneBasic_Backcourt",
-        "ShotZoneBasic_In_The_Paint_Non_RA": "ShotZoneBasic_In The Paint (Non-RA)",
-        "ShotZoneBasic_Left_Corner_3": "ShotZoneBasic_Left Corner 3",
-        "ShotZoneBasic_Mid_Range": "ShotZoneBasic_Mid-Range",
-        "ShotZoneBasic_Restricted_Area": "ShotZoneBasic_Restricted Area",
-        "ShotZoneBasic_Right_Corner_3": "ShotZoneBasic_Right Corner 3",
-        "ShotZoneArea_Back_Court_BC": "ShotZoneArea_Back Court(BC)",
-        "ShotZoneArea_Center_C": "ShotZoneArea_Center(C)",
-        "ShotZoneArea_Left_Side_Center_LC": "ShotZoneArea_Left Side Center(LC)",
-        "ShotZoneArea_Left_Side_L": "ShotZoneArea_Left Side(L)",
-        "ShotZoneArea_Right_Side_Center_RC": "ShotZoneArea_Right Side Center(RC)",
-        "ShotZoneArea_Right_Side_R": "ShotZoneArea_Right Side(R)",
-        "ShotZoneRange_16_24_ft": "ShotZoneRange_16-24 ft.",
-        "ShotZoneRange_24_ft": "ShotZoneRange_24+ ft.",
-        "ShotZoneRange_8_16_ft": "ShotZoneRange_8-16 ft.",
-        "ShotZoneRange_Back_Court_Shot": "ShotZoneRange_Back Court Shot",
-        "ShotZoneRange_Less_Than_8_ft": "ShotZoneRange_Less Than 8 ft.",
-        "SeasonType_Playoffs": "SeasonType_Playoffs",
-        "SeasonType_Regular_Season": "SeasonType_Regular Season",
-        "Game_ID_Frequency": "Game ID_Frequency",
-        "Game_Event_ID_Frequency": "Game Event ID_Frequency",
-        "Player_ID_Frequency": "Player ID_Frequency",
-        "Year": "Year",
-        "Month": "Month",
-        "Day": "Day",
-        "Day_of_Week": "Day_of_Week"
-    })
-    # Make a prediction with the loaded model
-    yhat = model.predict(df)
-    # Return the prediction as an answer
-    return {"prediction": int(yhat.item())}  # Use item() to extract a single element
+    url = f"http://{PREDICTION_SERVICE_HOST}:{PREDICTION_SERVICE_PORT}/unsecure_predict"
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=item.dict())
+        return response.json()
 
 
 class SimplePredictInput(BaseModel):
     X_Location: float
     Y_Location: float
     Player_Index: int
-# endpoint with a description of Simple Predict for Frontend
-@app.post('/simple_predict', response_model=Dict[str, int], name="Simple prediction based on X_Location, Y_Location, and Player_Index.")
-def simple_predict(input_data: SimplePredictInput):
-    """
-    Simple endpoint for prediction based on X_Location, Y_Location, and
-    Player_Index.
 
-    Args:
-        input_data (SimplePredictInput): Input data containing X_Location,
-        Y_Location, and Player_Index.
 
-    Returns:
-        dict: A dictionary containing a random prediction (0 or 1).
-    """
-    # Extract input data
-    X_Location = input_data.X_Location
-    Y_Location = input_data.Y_Location
-    Player_Index = input_data.Player_Index
+@app.post('/simple_predict', name="Simple prediction based on X_Location, Y_Location, and Player_Index.")
+async def simple_predict(input_data: SimplePredictInput):
+    url = f"http://{PREDICTION_SERVICE_HOST}:{PREDICTION_SERVICE_PORT}/simple_predict"
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=input_data.dict())
+    return response.json()
 
-    # Dummy prediction function
-    def simple_predict_no_model(X_Location, Y_Location, Player_Index):
-        # Replace with actual machine learning model
-        return random.randint(0, 1)
-
-    # Generate a random prediction (0 or 1)
-    prediction = simple_predict_no_model(X_Location, Y_Location, Player_Index)
-    return {"prediction": prediction}
