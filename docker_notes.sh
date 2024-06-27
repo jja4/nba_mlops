d640864f9fc9b3f962cd0c365af2d24d432d2770
Joel Aftreth <joelaftreth@gmail.com>
2024-06-27 09:43:56 +0000

    updated readme with new endpoints and login procedure

--- docker_notes.sh
+++ docker_notes.sh
@@ -4,8 +4,12 @@ docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
 docker volume rm $(docker volume ls -q)
 
-#check users table in PostgreSQL docker container
+# check users table in PostgreSQL docker container
 docker exec -it nba_mlops_db_1 psql -U ubuntu -d nba_db
 SELECT * FROM users;
-SELECT id,prediction,user_verification FROM predictions;
+
+# check predictions table
+SELECT id, prediction, user_verification FROM predictions;
+
+# check logging table
 # SELECT * FROM logging;
