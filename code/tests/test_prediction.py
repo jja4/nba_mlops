5dc8d352a16b5cc9e04346e7964c2c28571904f6
Mihran Dovlatyan <mihrandovlatyan@gmail.com>
2024-06-30 18:10:15 +0200

    fix path

--- code/tests/test_prediction.py
+++ code/tests/test_prediction.py
@@ -65,7 +65,7 @@ def test_root_endpoint(client: TestClient):
 
 
 # Patch the model's predict method to return a fixed value
-@patch('prediction_service.predict')
+@patch('api.prediction_service.predict')
 def test_predict_endpoint_shot_made(mock_predict):
     """
     Test the predict endpoint of the NBA prediction API for a made shot prediction.
